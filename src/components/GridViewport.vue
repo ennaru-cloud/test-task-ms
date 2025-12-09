9a84898e35dfd65f4e0f7cd71f902b9aa86208ae
Anton Soshnikov <a.soshnikov@squaregps.com>
2025-12-10 03:03:15 +0500

    Some grid configs and styles
    added number column

--- src/components/GridViewport.vue
+++ src/components/GridViewport.vue
@@ -15,14 +15,23 @@ const props = defineProps({
 
 const storage = new TreeStore(props.data as TItem[])
 
-const columnDefs = ref([
-  { field: 'label', flex: 2, cellDataType: 'string' }
+const defaultColDef = ref<ColDef>({ flex: 1 })
+const columnDefs = ref<ColDef[]>([
+  {
+    headerName: '№ п/п',
+    lockPosition: "left",
+    resizable: false,
+    width: 100,
+    flex: 0,
+    cellClass: 'index-cell',
+    valueGetter: ({ node }) => (node?.rowIndex || 0) + 1
+  },
+  { field: 'label', headerName: 'Наименование', flex: 2, cellDataType: 'string' }
 ])
-
-const rowData = computed(() => storage.getAll())
-
 const autoGroupColumnDef = ref<ColDef>({
   headerName: "Категория",
+  sortable: false,
+  sort: "asc",
   field: "group",
   valueGetter: ({ data }) => storage.getChildren(data.id).length ? 'Группа' : 'Элемент',
   cellClass: ({ value }) => value == 'Группа' ? 'group': 'element',
@@ -30,7 +39,8 @@ const autoGroupColumnDef = ref<ColDef>({
     suppressCount: true,
   },
 });
-
+const groupDefaultExpanded = ref(-1)
+const rowData = computed<TItem[]|null>(() => storage.getAll())
 const getDataPath = ref<GetDataPath>((data:TItem) => storage.getAllParents(data.id).map(({ id }) => id+'').reverse())
 
 </script>
@@ -42,7 +52,9 @@ const getDataPath = ref<GetDataPath>((data:TItem) => storage.getAllParents(data.
     :rowData="rowData"
     :getDataPath="getDataPath"
     :columnDefs="columnDefs"
+    :defaultColDef="defaultColDef"
     :autoGroupColumnDef="autoGroupColumnDef"
+    :groupDefaultExpanded="groupDefaultExpanded"
     />
 </template>
 
@@ -59,6 +71,10 @@ const getDataPath = ref<GetDataPath>((data:TItem) => storage.getAllParents(data.
   padding: 0 8px;
 }
 
+.grid .ag-cell-value.index-cell {
+  font-weight: bold;
+  padding-left: 20px;
+}
 .grid .ag-cell-value .ag-cell-wrapper {
   padding-left: calc(var(--ag-indentation-level) * var(--ag-row-group-indent-size) / 3);
 }
